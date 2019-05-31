f972f6190fe6a9be187eba9cceb31b0d72e6f41c
pentas1150 <beewol@soongsil.ac.kr>
2019-05-31 17:17:45 +0900

    간단한 로그인 기능 추가

--- video-streaming/routes/index.js
+++ video-streaming/routes/index.js
@@ -7,6 +7,18 @@ require('dotenv').config();
 const videoLocation = process.env.VIDEO_PATH;
 
 router.get('/', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  console.log(`id: ${req.body.id}, pw: ${req.body.pw}`);
+  if(req.body.id==='admin' && req.body.pw==='admin')
+    res.redirect('/main');
+  else
+    res.send("<script>alert('unknown user'); window.location='/'</script>'");
+});
+
+router.get('/main', (req, res, next) => {
   fs.readdir(videoLocation, function(err, list) {
       if(err){
           throw err;
@@ -21,7 +33,7 @@ router.get('/', (req, res, next) => {
         }
       });
 
-      res.render('index', { filelist: videoList, ip: process.env.BASE_IP })
+      res.render('main', { filelist: videoList, ip: process.env.BASE_IP })
   });
 });
 
