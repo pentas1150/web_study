1d38edcf409b99678dfd8963f169f9df4aaa22b2
pentas1150 <beewol@soongsil.ac.kr>
2019-06-02 19:06:42 +0900

    admin 페이지 및 반응형 video 페이지 추가

--- video-streaming/routes/main.js
+++ video-streaming/routes/main.js
@@ -2,11 +2,7 @@ const express = require('express');
 const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
 const router = express.Router();
 const Videolist = require('../schemas/videolist');
-const fs = require('fs');
-const path = require('path');
 require('dotenv').config();
-
-const videoLocation = process.env.VIDEO_PATH;
 
 router.get('/', isLoggedIn, async(req, res, next) => {
   try {
@@ -16,47 +12,6 @@ router.get('/', isLoggedIn, async(req, res, next) => {
     console.error(err);
     next(err);
   }
-});
-
-router.get('/updateDB', isLoggedIn, async(req, res, next) => { //관리자용으로 뺄 예정
-  try {
-    const videolist = await Videolist.find();
-    videolist.forEach(async(video) => {
-      try{
-        await Videolist.remove(video);
-      } catch(err) {
-        console.error(err);
-        return next(err);
-      }
-    });
-  } catch(err) {
-    console.error(err);
-    return next(err);
-  }
-
-  fs.readdir(videoLocation, (err, list) => {
-    if(err) {
-        throw err;
-    }
-
-    list.forEach(async(fileName) => {
-      try {
-        const fileExt = path.extname(fileName).toLowerCase();
-        if(fileExt === '.mp4'){
-          const videoname = new Videolist({
-            filename: fileName,
-          });
-
-          await videoname.save();
-        }
-      } catch(error) {
-        console.error(error);
-        return next(error);
-      }
-    });
-  });
-
-  res.send("<script>alert('update successfully'); window.location='/main';</script>");
 });
 
 module.exports = router;
