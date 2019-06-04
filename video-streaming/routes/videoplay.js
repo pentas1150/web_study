e64a1b101cfb494a39046c47f694cac3d36fe426
pentas1150 <beewol@soongsil.ac.kr>
2019-06-04 20:01:22 +0900

    비디오 전송에서 fs.statSync -> fs.stat(비동기)로 변경

--- video-streaming/routes/videoplay.js
+++ video-streaming/routes/videoplay.js
@@ -10,35 +10,40 @@ router.get('/:video', isLoggedIn, (req, res, next) => {
     res.render('video', { user: req.user, ip: process.env.BASE_IP, videoname: req.params.video });
 });
 
-router.get('/show/:video', isLoggedIn, (req, res, next) => {
+router.get('/show/:video', (req, res, next) => {
     const path = `${videoLocation}/${req.params.video}`;
-    const stat = fs.statSync(path);
-    const fileSize = stat.size;
     const range = req.headers.range;
 
-    if (range) {
-        const parts = range.replace(/bytes=/, "").split("-");
-        const start = parseInt(parts[0], 10);
-        const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
-        const chunksize = (end-start)+1;
-        const file = fs.createReadStream(path, {start, end});
-        const head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        };
+    fs.stat(path, (err, stat) => {
+        if(err)
+            throw err;
+        
+        const fileSize = stat.size;
 
-        res.writeHead(206, head);
-        file.pipe(res);
-    } else {
-         const head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(path).pipe(res);
-    }
+        if (range) {
+            const parts = range.replace(/bytes=/, "").split("-");
+            const start = parseInt(parts[0], 10);
+            const end = parts[1] ? parseInt(parts[1], 10) : fileSize-1;
+            const chunksize = (end-start)+1;
+            const file = fs.createReadStream(path, {start, end});
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            };
+    
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+             const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(200, head);
+            fs.createReadStream(path).pipe(res);
+        }
+    });
 });
 
 module.exports = router;
